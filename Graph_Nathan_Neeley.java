ae1b22ec2eb0f8244d5a8c1302897e8805d226f8
NathanNeeley <37356069+NathanNeeley@users.noreply.github.com>
2020-04-26 12:11:44 -0400

    Update Graph_Nathan_Neeley.java

--- Graph_Nathan_Neeley.java
+++ Graph_Nathan_Neeley.java
@@ -1,10 +1,3 @@
-// Name:       Nathan Neeley
-// Class:      CS 5040
-// Term:       Spring 2020
-// Instructor: Dr. Haddad
-// Assignment: 7
-// IDE:        jGrasp
-
 import java.util.*;
 
 public interface Graph_Nathan_Neeley<V> //interface of all abstract method that will be implemented in AbstractGraph
