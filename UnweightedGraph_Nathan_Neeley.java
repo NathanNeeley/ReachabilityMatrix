0a6373ae759b9e6eab3779c3c75d896a678251e2
NathanNeeley <37356069+NathanNeeley@users.noreply.github.com>
2020-04-26 12:13:05 -0400

    Update UnweightedGraph_Nathan_Neeley.java

--- UnweightedGraph_Nathan_Neeley.java
+++ UnweightedGraph_Nathan_Neeley.java
@@ -1,10 +1,3 @@
-// Name:       Nathan Neeley
-// Class:      CS 5040
-// Term:       Spring 2020
-// Instructor: Dr. Haddad
-// Assignment: 7
-// IDE:        jGrasp
-
 import java.util.*;
 
 public class UnweightedGraph_Nathan_Neeley<V> extends AbstractGraph_Nathan_Neeley<V>
